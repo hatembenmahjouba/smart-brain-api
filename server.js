65531d26afc437a7914b4c0362e75b05718eb89d
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-08-20 14:17:02 +0200

    deploy smart-brain-api

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.json(db.users);
+  res.send('it is working');
 });
 
 app.post('/signin', handleSignin(db, bcrypt));
