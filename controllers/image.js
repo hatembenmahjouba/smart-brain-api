9509bcbdad1ebd726a5e448b9bc8f5e98bcfb533
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-08-20 14:08:09 +0200

    deploy smart-brain-api

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+
+const handleApiCall = (req, res) => {
+  const app = new Clarifai.App({
+    apiKey: process.env.CLARIFAI_KEY,
+  });
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('Unable to work with api'));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json('unable to get entries'));
+};
+
+module.exports = {
+  handleApiCall,
+  handleImage,
+};
---
+++ server.js
@@ -0,0 +1,53 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+const { handleSignin } = require('./controllers/signin');
+const { handleRegister } = require('./controllers/register');
+const { handleProfileGet } = require('./controllers/profile');
+const { handleImage, handleApiCall } = require('./controllers/image');
+
+dotenv.config();
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+  },
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json(db.users);
+});
+
+app.post('/signin', handleSignin(db, bcrypt));
+
+app.post('/register', (req, res) => {
+  handleRegister(req, res, db, bcrypt);
+});
+
+app.get('/profile/:id', (req, res) => {
+  handleProfileGet(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  handleImage(req, res, db);
+});
+app.post('/imageurl', (req, res) => {
+  handleApiCall(req, res);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`app is running on port ${PORT}`);
+});
